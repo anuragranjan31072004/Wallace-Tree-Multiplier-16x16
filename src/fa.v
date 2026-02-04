e48d861e2645f61de7b812efc447b7b5d64369b9
anuragranjan31072004 <agranjan2020@gmail.com>
2026-02-04 20:07:34 +0530

    Create fa.v

---
+++ src/fa.v
@@ -0,0 +1,10 @@
+module fa (
+ input wire a,
+ input wire b,
+ input wire cin,
+ output wire sum,
+ output wire cout
+);
+ assign sum = a ^ b ^ cin;
+ assign cout = (a & b) | (a & cin) | (b & cin);
+endmodule
